f1d720a8d903dff4491d28d575a82d0b28876a26
Dylan Gonzalez <dylangiv123@gmail.com>
2021-02-02 15:04:36 -0800

    status bar color fix

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,14 +3,9 @@ import configureStore from '../redux/store'
 
 const { store } = configureStore()
 
-const localURL = 'http://192.168.1.189:45455'
-//const localURL = 'http://192.168.0.22:45455'
-const joshHouse = 'http://192.168.50.53:45455'
 const herokuURL = 'https://kala-app-api.herokuapp.com/'
 
 let instance = axios.create({
-    //baseURL: localURL
-    //baseURL: joshHouse,
     baseURL: herokuURL,
 });
 
--- src/screens/Home/ProfileScreen.js
+++ src/screens/Home/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
-import { ActivityIndicator, List, Text } from 'react-native-paper';
+import { List } from 'react-native-paper';
 import { useSelector, useDispatch } from 'react-redux';
 import { AUTH_LOGOUT } from '../../redux/actions/types';
 import { lightTheme, darkTheme } from '../../global/';
@@ -20,28 +20,23 @@ const ProfileScreen = () => {
     const theme = useSelector(state => state.theme.theme)
     const user = useSelector(state => state.user.user)
 
-    if (!user) {
-        return (
-            <ActivityIndicator />
-        )
-    } else {
+    
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+            <View style={{ flex: 1 }}>
+                <List.Section>
+                    <List.Subheader>Profile</List.Subheader>
+                    <List.Item title={(user) ? (user.firstName) : ''} left={() => <List.Icon icon='account-circle' />} />
+                    <List.Item title={(user) ? (user.userName) : ''} left={() => <List.Icon icon='email' />} />
+                    <List.Subheader>Settings</List.Subheader>
 
-        return (
-            <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
-                <View style={{ flex: 1 }}>
-                    <List.Section>
-                        <List.Subheader>Profile</List.Subheader>
-                        <List.Item title={(user) ? (user.firstName) : ''} left={() => <List.Icon icon='account-circle' />} />
-                        <List.Item title={(user) ? (user.userName) : ''} left={() => <List.Icon icon='email' />} />
-                        <List.Subheader>Settings</List.Subheader>
-
-                        <List.Item title={(theme.dark) ? 'Light mode' : 'Dark mode'} onPress={() => {(!theme.dark) ? nextTheme = darkTheme : nextTheme = lightTheme; switchTheme(nextTheme) }} left={() => <List.Icon icon='lightbulb-on-outline'/>} />
-                        <List.Item title='Log out' onPress={logout} left={() => <List.Icon icon='logout-variant'/>} />
-                    </List.Section>
-                </View>
-            </SafeAreaView>
-        )
-    }
+                    <List.Item title={(theme.dark) ? 'Light mode' : 'Dark mode'} onPress={() => {(!theme.dark) ? nextTheme = darkTheme : nextTheme = lightTheme; switchTheme(nextTheme) }} left={() => <List.Icon icon='lightbulb-on-outline'/>} />
+                    <List.Item title='Log out' onPress={logout} left={() => <List.Icon icon='logout-variant'/>} />
+                </List.Section>
+            </View>
+        </SafeAreaView>
+    )
+    
 }
 
 export default ProfileScreen
